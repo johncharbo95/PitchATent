f4d1bfa933810bf06ecd9a7d72bcd9227df8ab48
snowstorm257 <31776853+snowstorm257@users.noreply.github.com>
2018-05-11 20:49:06 -0400

    finalized the unforgetables

--- PitchATent/Createpdf.cs
+++ PitchATent/Createpdf.cs
@@ -113,7 +113,7 @@ namespace PitchATent
 
             // Create footer
             Paragraph paragraph = section.Footers.Primary.AddParagraph();
-            paragraph.AddText("Labelle Tents Inc · Route 500 Ouest · Ontario · Canada");
+            paragraph.AddText("Charbonneau Vendette Solutions ");
             paragraph.Format.Font.Size = 9;
             paragraph.Format.Alignment = ParagraphAlignment.Center;
 
@@ -227,7 +227,7 @@ namespace PitchATent
                 row.HeadingFormat = true;
                 row.Format.Alignment = ParagraphAlignment.Center;
                 row.Format.Font.Bold = true;
-                row.Shading.Color = Colors.Beige;
+                row.Shading.Color = Colors.White;
                 row.Cells[b].AddParagraph("□");
                 row.Cells[b].Format.Font.Bold = true;
                 row.Cells[b].Format.Alignment = ParagraphAlignment.Center;
@@ -355,7 +355,7 @@ namespace PitchATent
                 row2.HeadingFormat = true;
                 row2.Format.Alignment = ParagraphAlignment.Center;
                 row2.Format.Font.Bold = true;
-                row2.Shading.Color = Colors.Beige;
+                row2.Shading.Color = Colors.White;
                 row2.Cells[b].AddParagraph("□");
                 row2.Cells[b].Format.Font.Bold = true;
                 row2.Cells[b].Format.Alignment = ParagraphAlignment.Center;
@@ -480,7 +480,7 @@ namespace PitchATent
                 row3.HeadingFormat = true;
                 row3.Format.Alignment = ParagraphAlignment.Center;
                 row3.Format.Font.Bold = true;
-                row3.Shading.Color = Colors.Beige;
+                row3.Shading.Color = Colors.White;
                 row3.Cells[b].AddParagraph("□");
                 row3.Cells[b].Format.Font.Bold = true;
                 row3.Cells[b].Format.Alignment = ParagraphAlignment.Center;
@@ -609,7 +609,7 @@ namespace PitchATent
                 row4.HeadingFormat = true;
                 row4.Format.Alignment = ParagraphAlignment.Center;
                 row4.Format.Font.Bold = true;
-                row4.Shading.Color = Colors.Beige;
+                row4.Shading.Color = Colors.White;
                 row4.Cells[b].AddParagraph("□");
                 row4.Cells[b].Format.Font.Bold = true;
                 row4.Cells[b].Format.Alignment = ParagraphAlignment.Center;
@@ -737,7 +737,7 @@ namespace PitchATent
                 row5.HeadingFormat = true;
                 row5.Format.Alignment = ParagraphAlignment.Center;
                 row5.Format.Font.Bold = true;
-                row5.Shading.Color = Colors.Beige;
+                row5.Shading.Color = Colors.White;
                 row5.Cells[b].AddParagraph("□");
                 row5.Cells[b].Format.Font.Bold = true;
                 row5.Cells[b].Format.Alignment = ParagraphAlignment.Center;
@@ -828,25 +828,25 @@ namespace PitchATent
             Column column6 = this.table1.AddColumn("0.75cm");
             column.Format.Alignment = ParagraphAlignment.Center;
 
-            column6 = this.table1.AddColumn("4cm");
+            column6 = this.table1.AddColumn("3cm");
             column6.Format.Alignment = ParagraphAlignment.Right;
 
             column6 = this.table1.AddColumn("0.75cm");
             column6.Format.Alignment = ParagraphAlignment.Right;
 
-            column6 = this.table1.AddColumn("4cm");
+            column6 = this.table1.AddColumn("3.8cm");
             column6.Format.Alignment = ParagraphAlignment.Right;
 
             column6 = this.table1.AddColumn("0.75cm");
             column6.Format.Alignment = ParagraphAlignment.Right;
 
-            column6 = this.table1.AddColumn("4cm");
+            column6 = this.table1.AddColumn("3cm");
             column6.Format.Alignment = ParagraphAlignment.Right;
 
             column6 = this.table1.AddColumn("0.75cm");
             column6.Format.Alignment = ParagraphAlignment.Right;
 
-            column6 = this.table1.AddColumn("4cm");
+            column6 = this.table1.AddColumn("3.8cm");
             column6.Format.Alignment = ParagraphAlignment.Right;
 
             //----------------------------------------------------------------------------
@@ -857,72 +857,69 @@ namespace PitchATent
             row6.HeadingFormat = true;
             row6.Format.Alignment = ParagraphAlignment.Center;
             row6.Format.Font.Bold = true;
-            row6.Shading.Color = Colors.Cornsilk;
+            row6.Shading.Color = Colors.White;
 
             row6.Cells[0].AddParagraph("□");
             row6.Cells[0].Format.Font.Bold = true;
             row6.Cells[0].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[1].AddParagraph("Tarp");
+            row6.Cells[1].AddParagraph("Blue Tarp");
             row6.Cells[1].Format.Alignment = ParagraphAlignment.Center;
 
             row6.Cells[2].AddParagraph("□");
             row6.Cells[2].Format.Font.Bold = true;
             row6.Cells[2].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[3].AddParagraph("Arrache Pin");
+            row6.Cells[3].AddParagraph("Clips");
             row6.Cells[3].Format.Alignment = ParagraphAlignment.Center;
 
             row6.Cells[4].AddParagraph("□");
             row6.Cells[4].Format.Font.Bold = true;
             row6.Cells[4].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[5].AddParagraph("Masse");
+            row6.Cells[5].AddParagraph("Patch Kit");
             row6.Cells[5].Format.Alignment = ParagraphAlignment.Center;
 
             row6.Cells[6].AddParagraph("□");
             row6.Cells[6].Format.Font.Bold = true;
             row6.Cells[6].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[7].AddParagraph("Patching Tape");
+            row6.Cells[7].AddParagraph("Jack Hammer/Fuel");
             row6.Cells[7].Format.Alignment = ParagraphAlignment.Center;
 
             row6 = table1.AddRow();
             row6.HeadingFormat = true;
             row6.Format.Alignment = ParagraphAlignment.Center;
             row6.Format.Font.Bold = true;
-            row6.Shading.Color = Colors.Cornsilk;
+            row6.Shading.Color = Colors.White;
 
             row6.Cells[0].AddParagraph("□");
             row6.Cells[0].Format.Font.Bold = true;
             row6.Cells[0].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[1].AddParagraph("Tarp");
+            row6.Cells[1].AddParagraph("Spray 9/Rags");
             row6.Cells[1].Format.Alignment = ParagraphAlignment.Center;
 
             row6.Cells[2].AddParagraph("□");
             row6.Cells[2].Format.Font.Bold = true;
             row6.Cells[2].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[3].AddParagraph("Arrache Pin");
+            row6.Cells[3].AddParagraph("Sledge Hammer");
             row6.Cells[3].Format.Alignment = ParagraphAlignment.Center;
 
             row6.Cells[4].AddParagraph("□");
             row6.Cells[4].Format.Font.Bold = true;
             row6.Cells[4].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[5].AddParagraph("Masse");
+            row6.Cells[5].AddParagraph("Step Ladder");
             row6.Cells[5].Format.Alignment = ParagraphAlignment.Center;
 
             row6.Cells[6].AddParagraph("□");
             row6.Cells[6].Format.Font.Bold = true;
             row6.Cells[6].Format.Alignment = ParagraphAlignment.Center;
 
-            row6.Cells[7].AddParagraph("Patching Tape");
+            row6.Cells[7].AddParagraph("Impact Screws");
             row6.Cells[7].Format.Alignment = ParagraphAlignment.Center;
-
-
-            this.table.SetEdge(0, 0, 3, 1, Edge.Box, BorderStyle.Single, 1, Color.Empty);
 
             // Add the notes paragraph
             paragraph = section.AddParagraph();
@@ -936,158 +933,7 @@ namespace PitchATent
 
         }
 
-        //------//-----------//----------//---------//----------//----------//--------//
-
-        /// <summary>
-        /// Creates the dynamic parts of the invoice.
-        /// </summary>
-        //void FillContent()
-        //{
-        //    // Fill address in address text frame
-        //    XPathNavigator item = SelectItem("/invoice/to");
-        //    Paragraph paragraph = this.addressFrame.AddParagraph();
-        //    paragraph.AddText(GetValue(item, "name/singleName"));
-        //    paragraph.AddLineBreak();
-        //    paragraph.AddText(GetValue(item, "address/line1"));
-        //    paragraph.AddLineBreak();
-        //    paragraph.AddText(GetValue(item, "address/postalCode") + " " + GetValue(item, "address/city"));
-
-        //    // Iterate the invoice items
-        //    double totalExtendedPrice = 0;
-        //    XPathNodeIterator iter = this.navigator.Select("/invoice/items/*");
-        //    while (iter.MoveNext())
-        //    {
-        //        item = iter.Current;
-        //        double quantity = GetValueAsDouble(item, "quantity");
-        //        double price = GetValueAsDouble(item, "price");
-        //        double discount = GetValueAsDouble(item, "discount");
-
-        //        // Each item fills two rows
-        //        Row row1 = this.table.AddRow();
-        //        Row row2 = this.table.AddRow();
-        //        row1.TopPadding = 1.5;
-        //        row1.Cells[0].Shading.Color = TableGray;
-        //        row1.Cells[0].VerticalAlignment = VerticalAlignment.Center;
-        //        row1.Cells[0].MergeDown = 1;
-        //        row1.Cells[1].Format.Alignment = ParagraphAlignment.Left;
-        //        row1.Cells[1].MergeRight = 3;
-        //        row1.Cells[5].Shading.Color = TableGray;
-        //        row1.Cells[5].MergeDown = 1;
-
-        //        row1.Cells[0].AddParagraph(GetValue(item, "itemNumber"));
-        //        paragraph = row1.Cells[1].AddParagraph();
-        //        paragraph.AddFormattedText(GetValue(item, "title"), TextFormat.Bold);
-        //        paragraph.AddFormattedText(" by ", TextFormat.Italic);
-        //        paragraph.AddText(GetValue(item, "author"));
-        //        row2.Cells[1].AddParagraph(GetValue(item, "quantity"));
-        //        row2.Cells[2].AddParagraph(price.ToString("0.00") + " €");
-        //        row2.Cells[3].AddParagraph(discount.ToString("0.0"));
-        //        row2.Cells[4].AddParagraph();
-        //        row2.Cells[5].AddParagraph(price.ToString("0.00"));
-        //        double extendedPrice = quantity * price;
-        //        extendedPrice = extendedPrice * (100 - discount) / 100;
-        //        row1.Cells[5].AddParagraph(extendedPrice.ToString("0.00") + " €");
-        //        row1.Cells[5].VerticalAlignment = VerticalAlignment.Bottom;
-        //        totalExtendedPrice += extendedPrice;
-
-        //        this.table.SetEdge(0, this.table.Rows.Count - 2, 6, 2, Edge.Box, BorderStyle.Single, 0.75);
-        //    }
-
-        //    // Add an invisible row as a space line to the table
-        //    Row row = this.table.AddRow();
-        //    row.Borders.Visible = false;
-
-        //    // Add the total price row
-        //    row = this.table.AddRow();
-        //    row.Cells[0].Borders.Visible = false;
-        //    row.Cells[0].AddParagraph("Total Price");
-        //    row.Cells[0].Format.Font.Bold = true;
-        //    row.Cells[0].Format.Alignment = ParagraphAlignment.Right;
-        //    row.Cells[0].MergeRight = 4;
-        //    row.Cells[5].AddParagraph(totalExtendedPrice.ToString("0.00") + " €");
-
-        //    // Add the VAT row
-        //    row = this.table.AddRow();
-        //    row.Cells[0].Borders.Visible = false;
-        //    row.Cells[0].AddParagraph("VAT (19%)");
-        //    row.Cells[0].Format.Font.Bold = true;
-        //    row.Cells[0].Format.Alignment = ParagraphAlignment.Right;
-        //    row.Cells[0].MergeRight = 4;
-        //    row.Cells[5].AddParagraph((0.19 * totalExtendedPrice).ToString("0.00") + " €");
-
-        //    // Add the additional fee row
-        //    row = this.table.AddRow();
-        //    row.Cells[0].Borders.Visible = false;
-        //    row.Cells[0].AddParagraph("Shipping and Handling");
-        //    row.Cells[5].AddParagraph(0.ToString("0.00") + " €");
-        //    row.Cells[0].Format.Font.Bold = true;
-        //    row.Cells[0].Format.Alignment = ParagraphAlignment.Right;
-        //    row.Cells[0].MergeRight = 4;
-
-        //    // Add the total due row
-        //    row = this.table.AddRow();
-        //    row.Cells[0].AddParagraph("Total Due");
-        //    row.Cells[0].Borders.Visible = false;
-        //    row.Cells[0].Format.Font.Bold = true;
-        //    row.Cells[0].Format.Alignment = ParagraphAlignment.Right;
-        //    row.Cells[0].MergeRight = 4;
-        //    totalExtendedPrice += 0.19 * totalExtendedPrice;
-        //    row.Cells[5].AddParagraph(totalExtendedPrice.ToString("0.00") + " €");
-
-        //    // Set the borders of the specified cell range
-        //    this.table.SetEdge(5, this.table.Rows.Count - 4, 1, 4, Edge.Box, BorderStyle.Single, 0.75);
-
-        //    // Add the notes paragraph
-        //    paragraph = this.document.LastSection.AddParagraph();
-        //    paragraph.Format.SpaceBefore = "1cm";
-        //    paragraph.Format.Borders.Width = 0.75;
-        //    paragraph.Format.Borders.Distance = 3;
-        //    paragraph.Format.Borders.Color = TableBorder;
-        //    paragraph.Format.Shading.Color = TableGray;
-        //    item = SelectItem("/invoice");
-        //    paragraph.AddText(GetValue(item, "notes"));
-        //}
-
-        /// <summary>
-        /// Selects a subtree in the XML data.
-        /// </summary>
-        /// 
-        //XPathNavigator SelectItem(string path)
-        //{
-        //    XPathNodeIterator iter = this.navigator.Select(path);
-        //    iter.MoveNext();
-        //    return iter.Current;
-        //}
-
-        /// <summary>
-        /// Gets an element value from the XML data.
-        /// </summary>
-        //static string GetValue(XPathNavigator nav, string name)
-        //{
-        //    //nav = nav.Clone();
-        //    XPathNodeIterator iter = nav.Select(name);
-        //    iter.MoveNext();
-        //    return iter.Current.Value;
-        //}
-
-        ///// <summary>
-        ///// Gets an element value as double from the XML data.
-        ///// </summary>
-        //static double GetValueAsDouble(XPathNavigator nav, string name)
-        //{
-        //    try
-        //    {
-        //        string value = GetValue(nav, name);
-        //        if (value.Length == 0)
-        //            return 0;
-        //        return Double.Parse(value, CultureInfo.InvariantCulture);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Debug.WriteLine(ex.Message);
-        //    }
-        //    return 0;
-        //}
+        //---------------------------------------------------------------------------------//
 
         // Some pre-defined colors
 #if true
